3d055372063764eb0c4ec3c96d34dc3ef963cf8e
Philipp <philvas13@gmail.com>
2022-03-05 12:32:12 +0300

    add delete option

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -3,9 +3,10 @@ import java.util.List;
 
 public class Graph {
     private int maxVertexCount;
-    private int[][] connectionMatrix;
+    //    private int[][] connectionMatrix;
+    private List<List<Integer>> connectionMatrix;
     //    Vertex[] vertexList;
-    List<Vertex> vertexList;
+    private List<Vertex> vertexList;
     private int currentVertexIndex;
     private GraphStack graphStack;
 
@@ -13,9 +14,16 @@ public class Graph {
 //        this.vertexList = new Vertex[maxVertexCount];
         this.vertexList = new ArrayList<>();
         this.maxVertexCount = maxVertexCount;
-        this.connectionMatrix = new int[maxVertexCount][maxVertexCount];
+//        this.connectionMatrix = new int[maxVertexCount][maxVertexCount];
+        this.connectionMatrix = new ArrayList<>();
         this.currentVertexIndex = 0;
         this.graphStack = new GraphStack(maxVertexCount);
+        for (int t = 0; t < maxVertexCount; t++) {
+            connectionMatrix.add(new ArrayList<>());
+            for (int i = 0; i < maxVertexCount; i++) {
+                connectionMatrix.get(t).add(0);
+            }
+        }
     }
 
     public void addVertex(String name) {
@@ -26,28 +34,58 @@ public class Graph {
     public void addArc(String start, String end, int weight, boolean goBack) {
         Integer startIndex = 0;
         Integer endIndex = 0;
-        for (Vertex vertex: vertexList){
-            if(vertex.getName().equals(start)){
+        for (Vertex vertex : vertexList) {
+            if (vertex.getName().equals(start)) {
                 startIndex = vertexList.indexOf(vertex);
-            }else if(vertex.getName().equals(end)){
+            } else if (vertex.getName().equals(end)) {
                 endIndex = vertexList.indexOf(vertex);
             }
         }
-        connectionMatrix[startIndex][endIndex] = weight;
-        connectionMatrix[endIndex][startIndex] = goBack ? weight : -1;
+        connectionMatrix.get(startIndex).set(endIndex, weight);
+        if (goBack) {
+            connectionMatrix.get(endIndex).set(startIndex, weight);
+        } else {
+            connectionMatrix.get(endIndex).set(startIndex, -1);
+        }
+
+//        connectionMatrix[startIndex][endIndex] = weight;
+//        connectionMatrix[endIndex][startIndex] = goBack ? weight : -1;
     }
 
     public void deleteVertex(String name) {
-        for (Vertex vertex : vertexList) {
-            if (vertex.getName().equals(name)) {
-                vertexList.remove(vertex);
-            }
+        int vertexIndex = getVertexIndexByName(name);
+        vertexList.remove(getVertexIndexByName(name));
+//        int[][] newArr = new int[maxVertexCount][maxVertexCount];
+        for (List<Integer> list : connectionMatrix) {
+            list.remove(vertexIndex);
+            list.add(0);
         }
+        connectionMatrix.remove(vertexIndex);
+        connectionMatrix.add(new ArrayList<>());
+        for (int i = 0; i < maxVertexCount; i++) {
+            connectionMatrix.get(maxVertexCount - 1).add(0);
+        }
+//        for (int t = vertexIndex; t < maxVertexCount - 1; t++) {
+//            for (int i = vertexIndex; i < maxVertexCount - 1; i++) {
+//                newArr[t][i] = connectionMatrix[t + 1][i + 1];
+//            }
+//        }
+
+    }
+
+    public void deleteArc(String from, String to) {
+        int fromN = getVertexIndexByName(from);
+        int toN = getVertexIndexByName(to);
+        connectionMatrix.get(fromN).set(toN, 0);
+        connectionMatrix.get(toN).set(fromN, 0);
     }
 
     public int check(int vertex) {
         for (int i = 0; i < vertexList.size(); i++) {
-            if (connectionMatrix[vertex][i] != -1 && vertexList.get(i).isChecked() == false) {
+//            if (connectionMatrix[vertex][i] != -1 && vertexList.get(i).isChecked() == false) {
+//                return i;
+//            }
+            if (connectionMatrix.get(vertex).get(i) != -1 && vertexList.get(i).isChecked() == false) {
                 return i;
             }
         }
@@ -74,4 +112,52 @@ public class Graph {
             vertexList.get(i).setChecked(false);
         }
     }
+
+    public List<String> listOfInputArcs(String vertexName) {
+        List<String> inputArcs = new ArrayList<>();
+        for (int i = 0; i < maxVertexCount; i++) {
+//            if (connectionMatrix[i][getVertexIndexByName(vertexName)] > 0) {
+//                inputArcs.add(vertexList.get(i).getName());
+//            }
+            if (connectionMatrix.get(i).get(getVertexIndexByName(vertexName)) > 0) {
+                inputArcs.add(vertexList.get(i).getName());
+            }
+        }
+        return inputArcs;
+    }
+
+    public List<String> listOfOutputArcs(String vertexName) {
+        List<String> outputArcs = new ArrayList<>();
+        for (int i = 0; i < maxVertexCount; i++) {
+//            if (connectionMatrix[getVertexIndexByName(vertexName)][i] > 0) {
+//                outputArcs.add(vertexList.get(i).getName());
+//            }
+            if (connectionMatrix.get(getVertexIndexByName(vertexName)).get(i) > 0) {
+                outputArcs.add(vertexList.get(i).getName());
+            }
+        }
+        return outputArcs;
+    }
+
+    public int getVertexIndexByName(String name) {
+        for (int i = 0; i < maxVertexCount; i++) {
+            if (vertexList.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void printConnectionMatrix() {
+        for (int i = 0; i < maxVertexCount; i++) {
+            for (int t = 0; t < maxVertexCount; t++) {
+                System.out.print(connectionMatrix.get(i).get(t) + " ");
+//                System.out.print(connectionMatrix[i][t] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Graph graph = new Graph(15);
 
         graph.addVertex("A");
@@ -8,14 +8,24 @@ public class Main {
         graph.addVertex("C");
         graph.addVertex("D");
         graph.addVertex("E");
-        graph.addVertex("F");
+//        graph.addVertex("F");
 
-        graph.addArc("A","B",3, true);
-        graph.addArc("B","C",3, true);
-        graph.addArc("C","D",3, true);
-        graph.addArc("A","E",3, true);
-        graph.addArc("E","F",3, true);
+        graph.addArc("A", "B", 1, false);
+        graph.addArc("A", "C", 2, false);
+        graph.addArc("A", "D", 3, false);
+        graph.addArc("B", "E", 4, false);
+//        graph.addArc("E", "F", 5, false);
 
         graph.goDeep(0);
+
+        graph.printConnectionMatrix();
+//        for (String arc : graph.listOfInputArcs("E")) {
+//            System.out.println(arc);
+//        }
+//        for (String arc : graph.listOfOutputArcs("E")) {
+//            System.out.println(arc);
+//        }
+        graph.deleteArc("B", "E");
+        graph.printConnectionMatrix();
     }
 }
