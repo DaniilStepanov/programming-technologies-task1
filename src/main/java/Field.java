258c0fe5eb35c7f4562952c337270a12f3eac353
IrReiku <irreiku@mail.ru>
2022-04-07 16:56:41 +0300

    Сделаны тесты и исключения

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 public final class Field {
     public final int size; // длина стороны поля
     private final char[][] view;
+    private int theLongestCross;
+    private int theLongestNought;
 
     public Field(int size) {   // конструктор
         this.size = size;
@@ -25,37 +30,49 @@ public final class Field {
 
     }
 
-    public void addNought(int row, int column) {
+    public void addNought(int row, int column) throws Exception {
+        if (row < 1 || row > size || column < 1 || column > size) {
+            throw new Exception("Ошибка: В данном поле не существует такого ряда и/или столбца.");
+        } else if (view[row - 1][column - 1] != ' ') {
+            throw new Exception("Ошибка: Ячейка уже занята.");
+        }
         view[row - 1][column - 1] = 'O';
     }
 
-    public void addCross(int row, int column) {
+    public void addCross(int row, int column) throws Exception {
+        if (row < 1 || row > size || column < 1 || column > size) {
+            throw new Exception("Ошибка: В данном поле не существует такого ряда и/или столбца.");
+        } else if (view[row - 1][column - 1] != ' ') {
+            throw new Exception("Ошибка: Ячейка уже занята.");
+        }
         view[row - 1][column - 1] = 'X';
     }
 
-    public void clearCell(int row, int column) {
+    public void clearCell(int row, int column) throws Exception {
+        if (row < 1 || row > size || column < 1 || column > size) {
+            throw new Exception("Ошибка: В данном поле не существует такого ряда и/или столбца.");
+        }
         view[row - 1][column - 1] = ' ';
     }
 
-    public void theLongestCross() {
+    public int theLongestCross() {
         int[] count = {0, 0, 0, 0};
-        int max = -1;
         // проверка рядов и строк
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (view[i][j] == 'X') count[0]++;
                 else {
-                    if (count[0] > max) max = count[0];
+                    if (count[0] > theLongestCross) theLongestCross = count[0];
                     count[0] = 0;
                 }
                 if (view[j][i] == 'X') count[1]++;
                 else {
-                    if (count[1] > max) max = count[1];
+                    if (count[1] > theLongestCross) theLongestCross = count[1];
                     count[1] = 0;
                 }
             }
             for (int k = 0; k <= 1; k++) {
-                if (count[k] > max) max = count[k];
+                if (count[k] > theLongestCross) theLongestCross = count[k];
                 count[k] = 0;
             }
         }
@@ -64,51 +81,50 @@ public final class Field {
             for (int j = 0; j <= i; j++) {
                 if (view[j][i - j] == 'X') count[0]++;
                 else {
-                    if (count[0] > max) max = count[0];
+                    if (count[0] > theLongestCross) theLongestCross = count[0];
                     count[0] = 0;
                 }
                 if (view[size - 1 - j][size - 1 - i + j] == 'X') count[1]++;
                 else {
-                    if (count[1] > max) max = count[1];
+                    if (count[1] > theLongestCross) theLongestCross = count[1];
                     count[1] = 0;
                 }
                 if (view[j][size - 1 - i + j] == 'X') count[2]++;
                 else {
-                    if (count[2] > max) max = count[2];
+                    if (count[2] > theLongestCross) theLongestCross = count[2];
                     count[2] = 0;
                 }
                 if (view[size - 1 - j][i - j] == 'X') count[3]++;
                 else {
-                    if (count[3] > max) max = count[3];
+                    if (count[3] > theLongestCross) theLongestCross = count[3];
                     count[3] = 0;
                 }
             }
             for (int k = 0; k <= 3; k++) {
-                if (count[k] > max) max = count[k];
+                if (count[k] > theLongestCross) theLongestCross = count[k];
                 count[k] = 0;
             }
         }
-        System.out.println("Самая длинная последовательность крестиков: " + max);
+        return theLongestCross;
     }
-    public void theLongestNought() {
+    public int theLongestNought() {
         int[] count = {0, 0, 0, 0};
-        int max = -1;
         // проверка рядов и строк
         for (int row = 0; row < size; row++) {
             for (int column = 0; column < size; column++) {
                 if (view[row][column] == 'O') count[0]++;
                 else {
-                    if (count[0] > max) max = count[0];
+                    if (count[0] > theLongestNought) theLongestNought = count[0];
                     count[0] = 0;
                 }
                 if (view[column][row] == 'O') count[1]++;
                 else {
-                    if (count[1] > max) max = count[1];
+                    if (count[1] > theLongestNought) theLongestNought = count[1];
                     count[1] = 0;
                 }
             }
             for (int k = 0; k <= 1; k++) {
-                if (count[k] > max) max = count[k];
+                if (count[k] > theLongestNought) theLongestNought = count[k];
                 count[k] = 0;
             }
         }
@@ -117,37 +133,46 @@ public final class Field {
             for (int j = 0; j <= i; j++) {
                 if (view[j][i - j] == 'O') count[0]++;
                 else {
-                    if (count[0] > max) max = count[0];
+                    if (count[0] > theLongestNought) theLongestNought = count[0];
                     count[0] = 0;
                 }
                 if (view[size - 1 - j][size - 1 - i + j] == 'O') count[1]++;
                 else {
-                    if (count[1] > max) max = count[1];
+                    if (count[1] > theLongestNought) theLongestNought = count[1];
                     count[1] = 0;
                 }
                 if (view[j][size - 1 - i + j] == 'O') count[2]++;
                 else {
-                    if (count[2] > max) max = count[2];
+                    if (count[2] > theLongestNought) theLongestNought = count[2];
                     count[2] = 0;
                 }
                 if (view[size - 1 - j][i - j] == 'O') count[3]++;
                 else {
-                    if (count[3] > max) max = count[3];
+                    if (count[3] > theLongestNought) theLongestNought = count[3];
                     count[3] = 0;
                 }
             }
             for (int k = 0; k <= 3; k++) {
-                if (count[k] > max) max = count[k];
+                if (count[k] > theLongestNought) theLongestNought = count[k];
                 count[k] = 0;
             }
         }
-        System.out.println("Самая длинная последовательность крестиков: " + max);
+        return theLongestNought;
     }
-    /* private void objectInit (int row, int column, char object, int[] count, int place, int max) {
-        if (view[row][column] == object) count[place]++;
-        else {
-            if (count[place] > max) max = count[place];
-            count[place] = 0;
-        }
-    } */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Field field = (Field) o;
+        return size == field.size
+                && Arrays.deepEquals(view, field.view);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size, theLongestCross, theLongestNought);
+        result = 31 * result + Arrays.deepHashCode(view);
+        return result;
+    }
 }
